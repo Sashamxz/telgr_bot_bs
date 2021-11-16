8725cee93bf7197bd5ab009cd52b77393c332e24
Sashamxz <sasha.oom7@gmail.com>
2021-11-16 09:18:54 +0200

    add commands start/help and cenz_config

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
+from aiogram.types import message
 from aiogram.utils import executor
 from configs import TOKEN   
 # from client import  commands_start 
@@ -8,7 +9,8 @@ from configs import TOKEN
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot) 
 
-
+async def on_startup(_):
+    print('bot onlibe')
 
 
 
@@ -16,11 +18,27 @@ dp = Dispatcher(bot)
 async def command_start(message : types.Message):
     try:
         
-        await bot.send_message(f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+        await bot.send_message(message.from_user.id, f'Я бот. Hello, {message.from_user.first_name}')
         await message.delete()
     except:
         await message.reply('Общение с ботом через ЛС, написать : \nhttps://t.me/Pizza_HBot')    
 
+@dp.message_handler(commands=['Режим_роботи'])
+async def pizza_open_command(message : types.Message):
+	await bot.send_message(message.from_user.id, 'пн-чт з 9:00 до 20:00, пт-сб з 10:00 до 20:00, недiля вихiдний')
+
+@dp.message_handler(commands=['Розташування'])
+async def pizza_place_command(message : types.Message):
+	await bot.send_message(message.from_user.id, 'Street')
+
+
+
+@dp.message_handler(content_types=['text'])
+async def get_text_messages(message: types.Message):
+   if message.text.lower() == 'привет':
+       await message.answer('Привет!')
+   else:
+       await message.answer('Не понимаю, что это значит.')
 
 
 @dp.message_handler()
@@ -29,4 +47,4 @@ async def  echo(message : types.Message):
     
 
 
-executor.start_polling(dp, skip_updates=True)
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
